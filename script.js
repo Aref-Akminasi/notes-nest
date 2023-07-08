031e61ee1572cd9f851e8ebd559b6187078d245a
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-07-08 08:57:46 +0200

    added delete functionality

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ const colors = document.querySelectorAll('.color');
 const addNoteBtn = document.getElementById('add-note');
 const noteInput = document.getElementById('note-input');
 const nest = document.querySelector('.nest');
+let trash = '';
+let notes = '';
 let selectedColor = 'rgb(85, 107, 47)';
 let allNotes = [];
 
 function updateNotes() {
   try {
-    const notes = JSON.parse(localStorage.getItem('notes'));
-    for (let i = 0; i < notes.length; i++) {
-      allNotes.push(notes[i]);
-      nest.innerHTML += `<div class=note style="background-color:${notes[i][1]}" draggable="true">${notes[i][0]}</div>`;
+    const oldNotes = JSON.parse(localStorage.getItem('notes'));
+    for (let i = 0; i < oldNotes.length; i++) {
+      allNotes.push(oldNotes[i]);
+      nest.innerHTML += `<div class="note" style="background-color:${oldNotes[i][1]}" draggable="true">${oldNotes[i][0]} <i class="fa-sharp fa-solid fa-trash trash"></i></div>`;
     }
+    notes = document.querySelectorAll('.note');
+    trash = document.querySelectorAll('.trash');
   } catch {
     return false;
   }
@@ -19,11 +23,27 @@ function updateNotes() {
 
 updateNotes();
 
+if (trash.length > 0) {
+  trash.forEach((trash) => {
+    trash.addEventListener('click', deleteNote);
+  });
+}
+
 addNoteBtn.addEventListener('click', addNote);
 colors.forEach((color) => {
   color.addEventListener('click', selectColor);
 });
 
+function deleteNote() {
+  this.parentElement.remove();
+  allNotes = [];
+  notes = document.querySelectorAll('.note');
+  notes.forEach((note) => {
+    let data = [note.innerText, note.style.backgroundColor];
+    allNotes.push(data);
+  });
+  localStorage.setItem('notes', JSON.stringify(allNotes));
+}
 function selectColor() {
   colors.forEach((color) => {
     color.style.border = 'solid 0.1rem #fff';
@@ -33,8 +53,13 @@ function selectColor() {
 }
 
 function addNote() {
-  nest.innerHTML += `<div class=note style="background-color:${selectedColor}" draggable="true">${noteInput.value}</div>`;
+  nest.innerHTML += `<div class="note" style="background-color:${selectedColor}" draggable="true">${noteInput.value} <i class="fa-sharp fa-solid fa-trash trash"></i></div>`;
   let note = [noteInput.value, selectedColor];
   allNotes.push(note);
   localStorage.setItem('notes', JSON.stringify(allNotes));
+  notes = document.querySelectorAll('.note');
+  trash = document.querySelectorAll('.trash');
+  trash.forEach((trash) => {
+    trash.addEventListener('click', deleteNote);
+  });
 }
